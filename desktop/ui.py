382e0e27ec153211c5c327bf321260f304cd5bf2
ite_anjd <ite.anjd@outlook.com>
2017-12-31 19:55:30 +0200

    take some more!

--- desktop/ui.py
+++ desktop/ui.py
@@ -1,13 +1,17 @@
+import json
 import sys
 import threading
 from queue import Queue
+
+import cv2
 # from PySide import QtGui
 import qdarkstyle
-import cv2
 from PyQt5 import QtCore, QtGui, uic, QtWidgets
 from PyQt5.QtCore import Qt, QSize
 from PyQt5.QtGui import QPixmap, QIcon
+from PyQt5.QtWidgets import QLabel
 
+from controller.client import Client
 from controller.detectors.color_detector import ColorObjectDetector
 from controller.detectors.yolo_detector import YoloObjectDetector
 from controller.trackers.object_tracker import ObjectTracker
@@ -20,11 +24,16 @@ bboxes = None
 objects = None
 labels = None
 q = Queue()
-capture = cv2.VideoCapture(0)
+capture = None
 
 
-def setup_camera(width=1920, height=1080, fps=5):
+def setup_camera(url=None, width=1920, height=1080, fps=5):
     global capture
+    if url is None:
+        capture = cv2.VideoCapture(0)
+    else:
+        capture = cv2.VideoCapture(url)
+
     capture.set(cv2.CAP_PROP_FRAME_WIDTH, width)
     capture.set(cv2.CAP_PROP_FRAME_HEIGHT, height)
     capture.set(cv2.CAP_PROP_FPS, fps)
@@ -40,11 +49,18 @@ def grab(q):
 
 
 class Ui(QtWidgets.QMainWindow, FormClass):
-    def __init__(self, parent=None):
-        QtWidgets.QMainWindow.__init__(self, parent)
+    RUN = 'run'
+    STOP = 'stop'
+    MANUAL = 'manual'
+
+    def __init__(self, host='raspberrypi', port=1234, url=None):
+        QtWidgets.QMainWindow.__init__(self)
         self.setupUi(self)
-        self.drawing_method = "matplotlib"
-        self.with_prop = True
+
+        self.trackButton.clicked.connect(self.start_tracking)
+        self.forceStopButton.clicked.connect(self.stop_tracking)
+        self.setButton.clicked.connect(self.robot_initializer)
+
         self.window_width = self.videoWidget.frameSize().width()
         self.window_height = self.videoWidget.frameSize().height()
         self.videoWidget = ImageWidget(self.videoWidget, self)
@@ -55,7 +71,8 @@ class Ui(QtWidgets.QMainWindow, FormClass):
         self.videoWidget.mousePressEvent = self.select_object
 
         self.set_image_on_button(self.captureButton, True)
-        setup_camera()
+        setup_camera(url)
+
         self.resume_camera()
         self.timer = QtCore.QTimer(self)
         self.timer.timeout.connect(self.update_frame)
@@ -66,6 +83,128 @@ class Ui(QtWidgets.QMainWindow, FormClass):
 
         self.detector = self.yolo_detector
         self.tracker = ObjectTracker()
+
+        self.label = QLabel("Initialization")
+        self.statusBar.addWidget(self.label)
+
+        self.client = Client(host=host, port=port)
+        self.status = self.STOP
+
+    def keyPressEvent(self, event1):
+        self.status = self.MANUAL
+        self.ui.label_2.setText(self.status)
+
+        verbose = {"FB": "", "LR": ""}
+        if event1.key() == QtCore.Qt.Key_W:
+            # print "Up pressed"
+            verbose["FB"] = "F"
+        if event1.key() == QtCore.Qt.Key_S:
+            # print "D pressed"
+            verbose["FB"] = "B"
+
+        if event1.key() == QtCore.Qt.Key_A:
+            # print "L pressed"
+            verbose["LR"] = "L"
+        if event1.key() == QtCore.Qt.Key_D:
+            # print "R pressed"
+            verbose["LR"] = "R"
+
+        json_data = json.dumps(verbose)
+        if verbose["LR"] != "" or verbose["FB"] != "":
+            print(verbose)
+            self.client.send(json_data)
+
+    def keyReleaseEvent(self, event):
+        verbose = {"FB": "", "LR": ""}
+        if event.key() == QtCore.Qt.Key_W:
+            # print "Up rel"
+            verbose["FB"] = "S"
+        if event.key() == QtCore.Qt.Key_S:
+            # print "D rel"
+            verbose["FB"] = "S"
+
+        if event.key() == QtCore.Qt.Key_A:
+            # print "L pressed"
+            verbose["LR"] = "S"
+        if event.key() == QtCore.Qt.Key_D:
+            # print "R pressed"
+            verbose["LR"] = "S"
+
+        json_data = json.dumps(verbose)
+        if verbose["LR"] != "" or verbose["FB"] != "":
+            print(verbose)
+            self.client.send(json_data)
+            self.client.send(json_data)
+
+    def start_tracking(self):
+        self.status = self.RUN
+        if self.object_tracker.is_working:
+            print('start tracking')
+            verbose = {
+                "status": self.RUN
+            }
+            json_data = json.dumps(verbose)
+            self.client.send(json_data)
+
+            self.ui.label_2.setText(self.status)
+            data_sender_thread = threading.Thread(target=self.data_sender)
+            data_sender_thread.start()
+
+    def stop_tracking(self):
+        self.status = self.STOP
+        verbose = {
+            "status": self.STOP
+        }
+        json_data = json.dumps(verbose)
+        self.client.send(json_data)
+        self.ui.label_2.setText(self.status)
+
+    def robot_initializer(self):
+
+        try:
+            x_min = round(float(self.ui.lineEdit_5.text()), 2)
+        except:
+            x_min = 200
+
+        try:
+            x_max = round(float(self.ui.lineEdit_2.text()), 2)
+        except:
+            x_max = 300
+        try:
+            minArea = round(float(self.ui.lineEdit_3.text()), 2)
+        except:
+            minArea = 20
+        try:
+            maxArea = round(float(self.ui.lineEdit_4.text()), 2)
+        except:
+            maxArea = 100
+
+        verbose = {
+            "x_min": x_min,
+            "x_max": x_max,
+            "maxArea": maxArea,
+            "minArea": minArea,
+        }
+        json_data = json.dumps(verbose)
+        print(json_data)
+        self.client.send(json_data)
+
+    def data_sender(self):
+        verbose = {}
+        while self.object_tracker.is_working and self.status != self.STOP:
+            if len(self.object_tracker.positions) > 0:
+                currentPosition = self.object_tracker.positions[0]
+                if currentPosition[0] is not None:
+                    print(currentPosition)
+                    verbose["x"] = currentPosition[0]
+                    verbose["y"] = currentPosition[1]
+                    verbose["width"] = currentPosition[2]
+                    verbose["height"] = currentPosition[3]
+                    json_data = json.dumps(verbose)
+                    self.client.send(json_data)
+            time.sleep(0.2)
+        self.status = self.STOP
+        self.ui.label_2.setText(self.status)
 
     def set_yolo_detector(self):
         self.detector = self.yolo_detector
@@ -146,13 +285,27 @@ class Ui(QtWidgets.QMainWindow, FormClass):
         global running
         running = False
 
+        self.status = self.STOP
+        verbose = {
+            "status": self.STOP
+        }
+        json_data = json.dumps(verbose)
+        self.client.send(json_data)
+        QtCore.QCoreApplication.instance().quit()
+
+
+def main(ip, port, url=None):
+    app = QtWidgets.QApplication(sys.argv)
+    app.setStyleSheet(qdarkstyle.load_stylesheet_pyqt5())
+    ui = Ui(host=ip, port=port, url=url)
+    ui.showMaximized()
+
+    app.exec_()
+
 
 if __name__ == '__main__':
-    app = QtWidgets.QApplication(sys.argv)
-    # with open("qdarkstyle/style.qss") as f:
-    #     app.setStyleSheet(f.read())
+    # videoURL = "http://raspberrypi:8080/stream/video.mjpeg"
+    IP = "raspberrypi"
+    PORT = 1234
 
-    app.setStyleSheet(qdarkstyle.load_stylesheet_pyqt5())
-    ui = Ui()
-    ui.show()
-    app.exec_()
+    main(ip=IP, port=PORT, url=None)
